2f2c1b7472149c4b038bbcededf8ec28978116c5
Mihail Batalov <limecompro@gmail.com>
2022-04-23 00:58:29 +0500

    fixed some bugs

--- MusicDownloader/__main__.py
+++ MusicDownloader/__main__.py
@@ -7,6 +7,10 @@ from downloader import *
 
 SCRIPT_DIR = Path(__file__).parent.resolve()
 
+def track_exists(playlist, track):
+    path = Path(SCRIPT_DIR, '../converted_to_mp3/', playlist, track + '.mp3')
+    return path.is_file()
+
 def create_directories(name):
     dirs = (
         Path(SCRIPT_DIR, '../converted_to_mp3/'),
@@ -20,6 +24,9 @@ def create_directories(name):
             pass
 
 def main():
+
+    errors = []
+
     # read the names of playlists
     csv_files_parent_dir = Path('C:/Users/sadom/Desktop/spotify_playlists_to_download').resolve()
     csv_files = searching_all_files(csv_files_parent_dir)
@@ -38,6 +45,14 @@ def main():
 
         # download
         for query in tracks:
+
+            # skip existing tracks
+            if track_exists(playlist_name, query):
+                tracks_downloaded += 1
+                print(' [skipped] ({}/{}) {}'.format(tracks_downloaded, tracks_count, query))
+                continue
+
+            # find links for track and download
             link = search_video(query)
             audio_download_path = Path(SCRIPT_DIR, '../converted_to_mp3/', playlist_name)
             video_download_path = Path(SCRIPT_DIR, '../downloaded_mp4/', playlist_name)
@@ -45,9 +60,23 @@ def main():
             try:
                 download_audio_from_link(link=link, path=audio_download_path, filename=query)
                 print('({}/{}) {}'.format(tracks_downloaded, tracks_count, query))
-            except Exception:
-                download_video_from_link(link=link, path=video_download_path, filename=query)
-                print(' [video] ({}/{}) {}'.format(tracks_downloaded, tracks_count, query))
+            except Exception as exc1:
+                try:
+                    download_video_from_link(link=link, path=video_download_path, filename=query)
+                    print(' [video] ({}/{}) {}'.format(tracks_downloaded, tracks_count, query))
+                except Exception as exc2:
+                    print(' [error] Cannot download this track "{}"'.format(query))
+                    print(exc2)
+                    errors.append(query)
+
+    # print list of failed songs
+    if len(errors) > 0:
+        print()
+        print('These songs have not been downloaded:')
+        i = 1
+        for track in errors:
+            print(' {}. {}'.format(i, track))
+            i += 1
 
 if __name__ == '__main__':
     main()
