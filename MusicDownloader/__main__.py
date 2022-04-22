a487c4697ac075ef863838d4ab49cd9f58c934c0
Mihail Batalov <limecompro@gmail.com>
2022-04-22 22:42:09 +0500

    auto-creating directories for playlists

--- MusicDownloader/__main__.py
+++ MusicDownloader/__main__.py
@@ -7,15 +7,46 @@ from downloader import *
 
 SCRIPT_DIR = Path(__file__).parent.resolve()
 
+def create_directories(name):
+    dirs = (
+        Path(SCRIPT_DIR, '../converted_to_mp3/'),
+        Path(SCRIPT_DIR, '../downloaded_mp4/')
+    )
+    for path in dirs:
+        path = Path(path, name)
+        try:
+            Path.mkdir(path)
+        except FileExistsError:
+            pass
+
 def main():
-    query = 'Architects - Doomsday'
-    link = search_video(query)
-    try:
-        download_audio_from_link(link=link, filename=query)
-        print('downloaded audio ' + query)
-    except Exception:
-        download_video_from_link(link=link, filename=query)
-        print(' <!> downloaded video ' + query)
+    # read the names of playlists
+    csv_files_parent_dir = Path('C:/Users/sadom/Desktop/spotify_playlists_to_download').resolve()
+    csv_files = searching_all_files(csv_files_parent_dir)
+
+    for path in csv_files:
+        playlist_name = path.stem
+        create_directories(playlist_name)
+        
+        print('Downloading playlist "{}"...'.format(playlist_name))
+
+        # get the names of songs from csv file
+        tracks = read_csv_file(path)
+        tracks_count = len(tracks)
+        tracks_downloaded = 0
+
+        # download
+        for query in tracks:
+            link = search_video(query)
+            audio_download_path = Path(SCRIPT_DIR, '../converted_to_mp3/', playlist_name)
+            video_download_path = Path(SCRIPT_DIR, '../downloaded_mp4/', playlist_name)
+            tracks_downloaded += 1
+            try:
+                download_audio_from_link(link=link, path=audio_download_path, filename=query)
+                print('({}/{}) {}'.format(tracks_downloaded, tracks_count, query))
+            except Exception:
+                download_video_from_link(link=link, path=video_download_path, filename=query)
+                print(' [video] ({}/{}) {}'.format(tracks_downloaded, tracks_count, query))
 
 if __name__ == '__main__':
     main()
